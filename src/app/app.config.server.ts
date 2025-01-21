e5f49a75364e4977d2195d2548cff7549c29b895
zotov_ka <zotov_ka@pik.ru>
2025-01-21 18:47:39 +0300

    fix ssr errors

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import {UNIVERSAL_PROVIDERS} from '@ng-web-apis/universal';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    UNIVERSAL_PROVIDERS
   ]
 };
 
